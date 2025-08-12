83052623963dc7ed333471ff349002a82ca88de2
riyaaa19-cyber <riyaarya656@gmail.com>
2025-08-12 17:57:32 +0000

    datatypes

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,6 @@
+"use strict";// treat all JS code as newer version 
+// alert(3+3)//we are using nodejs, not browser
+let age = 34
+console.log(typeof age);
+console.log(typeof undefined);//undefined 
+console.log(type of null);//object
