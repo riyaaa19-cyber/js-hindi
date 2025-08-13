7195f0c40ea0d6274440b9dee552acb85711f0f9
riyaaa19-cyber <riyaarya656@gmail.com>
2025-08-13 19:04:55 +0000

    strings

---
+++ .devcontainer/01_basics/string.js
@@ -0,0 +1,38 @@
+// const name = "riya"
+// const repoCount = 50
+
+// console.log(`Hello my name is ${name} and my repoCount is ${repoCount}`);
+
+// const gameName = new String('riya')
+// console.log(gameName);
+// console.log(gameName[2]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.toLowerCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('a'));
+
+// const newString = gameName.substring(0,3)
+// console.log(newString);
+
+// const anotherString = gameName.slice(0,-3)
+// console.log(anotherString);
+
+
+const newStringOne = "  rii  "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://www.google.com/siuu%20riya"
+console.log(url.replace('%20', '-'));
+
+console.log(url.includes('hehe')); //false
+
+console.log(url.split('-'));
+
+console.log(url.small())
+
+
