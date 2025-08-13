725be9f5ad4d66053f1f5631d23199a04c32a407
riyaaa19-cyber <riyaarya656@gmail.com>
2025-08-13 18:38:59 +0000

    datatypes_summary.

--- .devcontainer/01_basics/datatypes_summary.js
+++ .devcontainer/01_basics/datatypes_summary.js
@@ -6,28 +6,48 @@
 // const isLoggedIn = false
 // const outsideTemp = null
 // let userEmail;
-const anotherId = symbol('221')
-const bigNumber = 3456777788n
+// const anotherId = symbol('221')
+// const bigNumber = 3456777788n
 // console.log(id === anotherId);
 
 
 //reference (non-primitive)
 //array , objects , functions
 //array
-const heros = ["shaktiman", "nagraj" , "doga"];
+// const heros = ["shaktiman", "nagraj" , "doga"];
 
-//objects
-let myObj = {
-    name: "riya",
-    age: 22,
-}
+// //objects
+// let myObj = {
+//     name: "riya",
+//     age: 22,
+// }
 
 
-//functions
-const myFunction = function()
-{
+// //functions
+// const myFunction = function()
+// {
 
-    console.log("hello worls");
+    
 
-}
-console.log(typeof bigNumber);
+//stack(primitive), copy you get
+
+// let myYoutubename = "riyyarya"
+// let anothername = myYoutubename
+// anothername = "riyaa"
+// console.log(myYoutubename);
+// console.log(anothername);
+
+//heap
+
+// let userOne = {
+//     email: "riya@gmail.com",
+//     upi: "riya@ybl"
+// }
+//  let userTwo = userOne;
+
+//  userTwo.email = "riyaa@gmail.com"
+// console.log(userOne.email); 
+// console.log(userTwo.email);
+
+
+
