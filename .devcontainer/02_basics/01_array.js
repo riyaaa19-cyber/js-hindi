d9b150fbb9de10cbe1830740243739e5a3711f25
riyaaa19-cyber <riyaarya656@gmail.com>
2025-08-13 20:59:58 +0000

    array

---
+++ .devcontainer/02_basics/01_array.js
@@ -0,0 +1,17 @@
+//array
+
+const myArr = [0,1,2,3,4,5]
+const myHeroes = ["shaktiman", "nagraj"]
+const myArr2 = new Array(1,2,3,4,5)
+console.log(myArr[0]);
+
+
+//array methods
+
+// myArr.push(6);
+// console.log(myArr);
+
+// myArr.pop()
+
+myArr.unshift(9)
+console.log(myArr);
