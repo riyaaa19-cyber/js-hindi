6ef64dd313b167389763180e74dd5f6167ecd130
riyaaa19-cyber <riyaarya656@gmail.com>
2025-08-13 18:25:01 +0000

    datatpes

---
+++ .devcontainer/01_basics/datatypes_summary.js
@@ -0,0 +1,33 @@
+// primitive data types
+
+//7types: string,number,boolean,null,undefined,symbol,BigInt
+
+// const score = 100
+// const isLoggedIn = false
+// const outsideTemp = null
+// let userEmail;
+const anotherId = symbol('221')
+const bigNumber = 3456777788n
+// console.log(id === anotherId);
+
+
+//reference (non-primitive)
+//array , objects , functions
+//array
+const heros = ["shaktiman", "nagraj" , "doga"];
+
+//objects
+let myObj = {
+    name: "riya",
+    age: 22,
+}
+
+
+//functions
+const myFunction = function()
+{
+
+    console.log("hello worls");
+
+}
+console.log(typeof bigNumber);
