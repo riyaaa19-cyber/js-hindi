18ea677a5ef2520766be5d8ee7aa275c1b6f5a49
riyaaa19-cyber <riyaarya656@gmail.com>
2025-08-15 10:15:55 +0000

    array

--- .devcontainer/02_basics/01_array.js
+++ .devcontainer/02_basics/01_array.js
@@ -1,9 +1,9 @@
 //array
 
-const myArr = [0,1,2,3,4,5]
-const myHeroes = ["shaktiman", "nagraj"]
-const myArr2 = new Array(1,2,3,4,5)
-console.log(myArr[0]);
+// const myArr = [0,1,2,3,4,5]
+// const myHeroes = ["shaktiman", "nagraj"]
+// const myArr2 = new Array(1,2,3,4,5)
+// console.log(myArr[0]);
 
 
 //array methods
@@ -13,5 +13,28 @@ console.log(myArr[0]);
 
 // myArr.pop()
 
-myArr.unshift(9)
-console.log(myArr);
+// myArr.unshift(9)
+// myArr.shift()
+
+// console.log(myArr.includes(9)); //false
+// console.log(myArr.indexOf(3)); //3
+
+// const newArr = myArr.join()
+
+// console.log(typeof newArr);
+// console.log(myArr);
+
+//slice,splice
+
+// console.log("A" , myArr);
+// const myn1 = myArr.slice(1, 3)
+// console.log(myn1);
+// console.log("B", myArr)
+
+// const myn2 = myArr.splice(1, 3)
+// console.log("C", myArr);
+// console.log(myn2);
+
+
+
+
